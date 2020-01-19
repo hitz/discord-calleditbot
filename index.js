df4a16b254e03581b5b3d7a01d22b7c47808c1c5
Ben Hitz <hitz@stanford.edu>
2020-01-19 10:21:57 -0800

    working called it bot for a single discord

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-require('dotenv').config()
-const Discord = require('discord.js')
-const client = new Discord.Client()
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+let botre = /^!(\S+) (.+?$)/;
+let calleditChannelId = '668515113904504863';
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`)
+});
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
-  }
-})
-
-client.login(process.env.BOT_TOKEN)
+bot.on('message', msg => {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (parsed_msg = botre.exec(msg.content)) {
+        let cmd = parsed_msg[1];
+        let v = parsed_msg[2];
+        //console.log(parsed_msg);
+        //console.log("checking:"+cmd+" "+v);
+        switch(cmd) {
+            // !calledit
+            case 'calledit':
+                //console.log(msg);
+                postData = `${msg.author} has CALLED IT: ${v}`;
+                msg.channel.send(postData);
+                cI = bot.channels.get(calleditChannelId)
+                if (cI == null) {
+                    console.log('No channel calledit found');
+                    //console.log(bot.channels)
+                } else {
+                    cI.send(postData);
+                }
+            break;
+            // Just add any case commands if you want to..
+            }
+        }
+    });
+bot.login(process.env.BOT_TOKEN);
 
